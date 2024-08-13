b9eac934a28e634a8fd10603382a85d0d85d0ddf
aqilahakmalia <aqilaakmalia98@gmail.com>
2024-08-13 20:57:29 +0700

    resolve error migration & seeder

--- database/seeders/StudentExtracurricularSeeder.php
+++ database/seeders/StudentExtracurricularSeeder.php
@@ -16,19 +16,69 @@ class StudentExtracurricularSeeder extends Seeder
     {
         $studentExtracurriculars = [
             [
-                'student_id' => 1, // ID siswa yang pertama
-                'extracurricular_id' => 1, // ID ekstrakurikuler yang pertama
+                'student_id' => 1,
+                'extracurricular_id' => 1,
                 'start_year' => 2020
             ],
             [
                 'student_id' => 1,
-                'extracurricular_id' => 2, // ID ekstrakurikuler yang kedua
+                'extracurricular_id' => 2,
                 'start_year' => 2023
             ],
             [
-                'student_id' => 2, // ID siswa yang kedua
+                'student_id' => 2,
                 'extracurricular_id' => 1,
                 'start_year' => 2024
+            ],
+            [
+                'student_id' => 2, 
+                'extracurricular_id' => 3, 
+                'start_year' => 2021
+            ],
+            [
+                'student_id' => 2,
+                'extracurricular_id' => 4, 
+                'start_year' => 2022
+            ],
+            [
+                'student_id' => 3, 
+                'extracurricular_id' => 5, 
+                'start_year' => 2023
+            ],
+            [
+                'student_id' => 3,
+                'extracurricular_id' => 6, 
+                'start_year' => 2024
+            ],
+            [
+                'student_id' => 4, 
+                'extracurricular_id' => 7, 
+                'start_year' => 2021
+            ],
+            [
+                'student_id' => 4,
+                'extracurricular_id' => 8, 
+                'start_year' => 2022
+            ],
+            [
+                'student_id' => 5, 
+                'extracurricular_id' => 9, 
+                'start_year' => 2023
+            ],
+            [
+                'student_id' => 5,
+                'extracurricular_id' => 10, 
+                'start_year' => 2024
+            ],
+            [
+                'student_id' => 6, 
+                'extracurricular_id' => 1, 
+                'start_year' => 2022
+            ],
+            [
+                'student_id' => 6,
+                'extracurricular_id' => 3, 
+                'start_year' => 2023
             ],
         ];
 
